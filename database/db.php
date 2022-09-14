2e1114a6b147fe59e550d3d2f53cea02c9173a82
Jonathan Esponda <jonaesponda.92@gmail.com>
2022-09-14 17:37:11 -0300

    Trabajando con mensajes

--- database/db.php
+++ database/db.php
@@ -1,9 +1,9 @@
 
 <?php 
-   $conexion = mysqli_connect(
-    'localhost',
-    'root',
-    '',
+  $conexion = mysqli_connect(
+    '192.168.1.50:3306',
+    'ceilab',
+    'ceilab',
     'db_ceilab'
     );
     
